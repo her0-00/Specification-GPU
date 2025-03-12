005a16adafa776c8571c7eb70109e93ad8d25cde
Specification GPU <138667514+her0-00@users.noreply.github.com>
2025-03-12 17:59:13 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,7 +8,9 @@ ui <- shinyUI(dashboardPage(
       menuItem("Présentation des données", tabName = "presentation", icon = icon("table")),
       menuItem("Analyses descriptives", tabName = "resume", icon = icon("file-alt")),
       menuItem("GPU PC", tabName = "GPUP", icon = icon("chart-bar")),
-      menuItem("GPU laptop", tabName = "GPUL", icon = icon("chart-bar"))
+      menuItem("GPU laptop", tabName = "GPUL", icon = icon("chart-bar")),
+      menuItem("ACP", tabName = "acp", icon = icon("chart-line")),
+      menuItem("Etude Technique", tabName = "Etude_technique", icon = icon("cogs"))
     ),
     selectInput(
       inputId = "IGP",
@@ -38,78 +40,98 @@ ui <- shinyUI(dashboardPage(
   
   # Contenu du corps -----
   dashboardBody(
+    useShinyjs(),
+    
     tags$head(
-      tags$link(rel = "stylesheet", type = "text/css", href = "www/styles.css"),
-      tags$style(HTML("
-        body, .content-wrapper, .main-header, .main-sidebar {
-          font-family: 'Times New Roman', Times, serif;
-          background-color: #808080; /* Fond gris */
-          color: #ffffff;
-        }
-        .skin-blue .main-header .box {
-          background-color: #696969;
-        }
-        .skin-blue .main-header .navbar {
-          background-color: #696969;
-        }
-        .skin-blue .main-header .logo {
-          background-color: #696969;
-          color: #ffffff;
-          border-bottom: 0 solid transparent;
-        }
-        .skin-blue .main-header .logo:hover {
-          background-color: #696969;
-        }
-        .skin-blue .main-sidebar {
-          background-color: #696969;
-        }
-        .skin-blue .main-sidebar .sidebar .sidebar-menu .active a {
-          background-color: #808080;
-        }
-        .skin-blue .main-sidebar .sidebar .sidebar-menu a {
-          background-color: #696969;
-          color: #ffffff;
-        }
-        .skin-blue .main-sidebar .sidebar .sidebar-menu a:hover {
-          background-color: #808080;
-        }
-        .skin-blue .main-header .navbar .sidebar-toggle {
-          color: #ffffff;
-        }
-        .skin-blue .main-header .navbar .sidebar-toggle:hover {
-          background-color: #808080;
-        }
-        .centered {
-          text-align: center;
-        }
-        .box-centered {
-          display: flex;
-          justify-content: center;
-        }
-        .input-centered {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        .box-centered {
-          display: flex;
-          justify-content: center;
-        }
-        .descriptive-text {
-          padding: 20px;
-          border-radius: 10px;
-          background: linear-gradient(to right, black, grey);
-        }
-        .boxplot-box {
-          color: white;
-        }
-        .box {
-          background: linear-gradient(to right, black, grey);
-        }
-        .box-primary {
-          background-color: grey !important;
-        }
-      "))
+      tags$style(HTML(
+        "body {font-family: 'Arial', sans-serif; background-color: #F4F6F9; color: #333;}
+        .box-title { font-weight: bold; font-size: 16px; text-transform: uppercase; }
+        .skin-blue .main-header .logo, .skin-blue .main-header .navbar { background-color: #3271a5; }
+        .skin-blue .main-sidebar { background-color: #3271a5; }
+        .box { border-radius: 12px; box-shadow: 3px 3px 15px rgba(0, 0, 0, 0.15); transition: 0.3s ease-in-out; }
+        .box:hover { transform: none !important ; }
+        .btn { transition: background-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out; border-radius: 8px; }
+        .btn:hover { background-color: #ff9800 !important; color: white !important; box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3); }
+        .dark-mode { background-color: #2C3E50; color: white; }
+        .dark-mode .box { background-color: #34495E; color: white; border-color: #555; }
+        .dark-mode .btn { background-color: #5DADE2 !important; color: white !important; }
+        .dark-mode .sidebar { background-color: #1A252F !important; }
+        .dark-mode .main-header, .dark-mode .navbar { background-color: #1F2933; }
+        .dark-mode .sidebar-menu > li > a { color: white !important; }
+        .dark-mode .sidebar-menu > li.active > a { background-color: #445A6F !important; }
+        .tab-pane { padding: 15px; }
+        .box-body { padding: 15px; }
+        .footer { background-color: #222D32; color: white; padding: 15px; text-align: center; font-size: 14px; }
+        .nav-tabs-custom .nav-tabs li a {
+      font-size: 15px;
+      font-weight: bold;
+      color: #3271a5;
+      padding: 10px 15px;
+      border-radius: 8px;
+      transition: all 0.3s ease-in-out;
+    }
+    
+    .nav-tabs-custom .nav-tabs li a:hover {
+      background-color: #ff9800;
+      color: white !important;
+      border-radius: 8px;
+    }
+
+    .nav-tabs-custom .nav-tabs .active a, 
+    .nav-tabs-custom .nav-tabs .active a:hover {
+      background-color: #3271a5 !important;
+      color: white !important;
+      border-radius: 8px;
+    }
+
+    .nav-tabs {
+      border-bottom: 2px solid #3271a5;
+      margin-bottom: 15px;
+    }
+        /* Style général des onglets */
+    .nav-tabs-custom .nav-tabs li a {
+      font-size: 16px;
+      font-weight: bold;
+      color: #3271a5;
+      padding: 12px 18px;
+      border-radius: 8px;
+      transition: all 0.3s ease-in-out;
+      text-transform: uppercase;
+      letter-spacing: 1px;
+    }
+
+
+
+    /* Style de l'onglet actif */
+    .nav-tabs-custom .nav-tabs .active a, 
+    .nav-tabs-custom .nav-tabs .active a:hover {
+      background-color: #3271a5 !important;
+      color: white !important;
+      border-radius: 8px;
+      box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
+      position: relative;
+    }
+      
+    .selectize-dropdown {
+      z-index: 10000 !important; /* S'assure que la liste est bien au-dessus */
+      position: absolute !important; /* Fixe la position */
+    }
+    
+    .selectize-dropdown-content {
+      position: relative !important; /* Empêche le flottement de la liste */
+    }
+    
+    .box {
+      overflow: visible !important; /* S'assure que la box ne cache pas la liste */
+    }
+    
+    .selectize-input {
+      cursor: pointer !important; /* Améliore la sélection */
+    }
+
+
+        "
+      ))
     ),
     tabItems(
       # Première section : Présentation des données -----
@@ -184,7 +206,82 @@ ui <- shinyUI(dashboardPage(
                   )
                 )
               )
-      )
+      ),
+      
+      # Onglet ACP
+      tabItem(tabName = "acp",
+              tabsetPanel(
+                tabPanel("Affichage des projections",
+                         fluidRow(
+                           box(
+                             title = "Sélectionnez les variables pour l'ACP", status = "primary", solidHeader = TRUE, width = 6,
+                             selectInput("acp_vars", "Variables actives :", choices = quant_vars_actives, selected = quant_vars_actives[1:3], multiple = TRUE),
+                             actionButton("run_acp", "Lancer l'ACP", class = "btn-success"),
+                             actionButton("select_all_AC", "Sélectionner toutes les variables", class = "btn-info")
+                           ),
+                           column(6,
+                                  box(
+                                    title = "Sélectionnez les variables catégorielles", status = "primary", solidHeader = TRUE, width = 12,
+                                    selectInput("acp_cat_vars", "Variables catégorielles :", choices = cat_vars, selected = NULL, multiple = TRUE),
+                                    actionButton("apply_cat", "Appliquer", class = "btn-success")
+                                  )
+                           )
+                         ),
+                         fluidRow(
+                           column(6, box(title = "Projection des individus", status = "primary", solidHeader = TRUE, width = 12, plotOutput("acp_ind_plot"))),
+                           column(6, box(title = "Projection des variables", status = "primary", solidHeader = TRUE, width = 12, plotOutput("acp_var_plot"))),
+                           column(6, box(title = "Résumé de l'ACP", status = "primary", solidHeader = TRUE, width = 12, verbatimTextOutput("acp_results_text"))),
+                           column(6, box(title = "BiPlot", status = "primary", solidHeader = TRUE, width = 12, plotOutput("biplot")))
+                         )
+                ),
+                tabPanel("Choix du nombre d'axes factorielles",
+                         box(title = "Choix du nombre d'axes factorielles", status = "primary", solidHeader = TRUE, width = 12,
+                             plotOutput("acp_eigenvalues"))
+                ),
+                tabPanel("Matrice des variances-covariances",
+                         box(title = "Matrice des variances-covariances", status = "primary", solidHeader = TRUE, width = 12,
+                             verbatimTextOutput("var_cov_matrix"))
+                ),
+                tabPanel("Valeurs propres",
+                         box(title = "Valeurs propres", status = "primary", solidHeader = TRUE, width = 12,
+                             verbatimTextOutput("eigen_values"))
+                ),
+                tabPanel("Vecteurs Propres",
+                         box(title = "Vecteurs propres", status = "primary", solidHeader = TRUE, width = 12,
+                             verbatimTextOutput("eigen_vectors"))
+                ),
+                tabPanel("Pourcentage d'inertie totale",
+                         box(title = "Pourcentage d'inertie totale", status = "primary", solidHeader = TRUE, width = 12, 
+                             verbatimTextOutput("inertia_percentage"))
+                )
+              )
+      ),
+      
+      # Onglet Étude Technique
+      tabItem(tabName = "Etude_technique",
+              tabsetPanel(
+                tabPanel("Contributions des variables",
+                         fluidRow(
+                           box(title = "Axe 1", status = "primary", solidHeader = TRUE, width = 12,
+                               plotOutput("contrib_PC1"))
+                         ),
+                         fluidRow(
+                           box(title = "Axe 2", status = "primary", solidHeader = TRUE, width = 12,
+                               plotOutput("contrib_PC2"))     
+                         )
+                ),
+                tabPanel("Qualité de la représentation des variables (cos²)",
+                         fluidRow(
+                           box(title = "Axe 1", status = "primary", solidHeader = TRUE, width = 12,
+                               plotOutput("cos2_PC1"))
+                         ),
+                         fluidRow(
+                           box(title = "Axe 2", status = "primary", solidHeader = TRUE, width = 12,
+                               plotOutput("cos2_PC2"))
+                         )
+                )
+              )
+      ) # Fin de l'onglet Étude Technique
     )
   )
 ))
